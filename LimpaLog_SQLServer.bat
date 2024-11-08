28886feba17109b98e0fda000ed2f76876595740
Gabriel Moreno Serignollli <92124202+Serignolli@users.noreply.github.com>
2024-11-08 16:00:21 -0300

    Update LimpaLog_SQLServer.bat

--- LimpaLog_SQLServer.bat
+++ LimpaLog_SQLServer.bat
@@ -4,7 +4,6 @@ set "DatabaseName=NOMEDABASE"
 set "UserName=SEU-USERNAME"
 set "UserPassword=SUA-SENHA"
 
-sqlcmd.exe -S %Instancia% -U %UserName% -P %UserPassword% -Q "USE [%DatabaseName%]; EXEC dmi_sp_ApagaTemporarias;"
 sqlcmd.exe -S %Instancia% -U %UserName% -P %UserPassword% -Q "USE [%DatabaseName%]; ALTER DATABASE %DatabaseName% SET RECOVERY SIMPLE; DBCC SHRINKFILE (N'%LogicalDatabaseName%' , 0, TRUNCATEONLY); ALTER DATABASE %DatabaseName% SET RECOVERY FULL;"
 
 pause
